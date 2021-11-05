b85a53533c7b181037345ccb37e9729016626363
Elon Gliksberg <elon@egk.local>
2021-11-05 18:12:24 +0200

    Removed show_capture and video is always on even if no hands detected.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ model = load_model('mp_hand_gesture')
 classNames = ['okay', 'peace', 'thumbs up', 'thumbs down', 'call me', 'stop', 'rock', 'live long', 'fist', 'smile']
 
 
-def start(video_path, show_capture):
+def start(video_path):
     cap = cv2.VideoCapture(video_path or 0)
     while True:
         _, frame = cap.read()
@@ -28,24 +28,19 @@ def start(video_path, show_capture):
         result = hands.process(framergb)
         className = ''
 
-        if not result.multi_hand_landmarks:
-            continue
-
-        for hand_landmarks in result.multi_hand_landmarks:
-            landmarks = [[landmark.x * frame_width, landmark.y * frame_height] for landmark in hand_landmarks.landmark]
-            prediction = model.predict([landmarks])
-            className = classNames[np.argmax(prediction)]
-
-            if show_capture:
+        if result.multi_hand_landmarks:
+            for hand_landmarks in result.multi_hand_landmarks:
+                print(hand_landmarks)
+                landmarks = [[landmark.x * frame_width, landmark.y * frame_height]
+                             for landmark in hand_landmarks.landmark]
+                prediction = model.predict([landmarks])
+                className = classNames[np.argmax(prediction)]
                 mpDraw.draw_landmarks(frame, hand_landmarks, mpHands.HAND_CONNECTIONS)
-            else:
-                print(f'Found {className} @ {int(cap.get(cv2.CAP_PROP_POS_MSEC) / 1000)}s')
 
-        if show_capture:
-            cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-            cv2.imshow("Output", frame)
-            if cv2.waitKey(1) == ord('q'):
-                break
+        cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.imshow("Output", frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
 
     cap.release()
     cv2.destroyAllWindows()
@@ -54,11 +49,9 @@ def start(video_path, show_capture):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--video-path', help='Path to a video.')
-    parser.add_argument('--show-capture', default=True, action=argparse.BooleanOptionalAction,
-                        help='Whether to display a window of the capture.')
     args = parser.parse_args()
 
-    start(args.video_path, args.show_capture)
+    start(args.video_path)
 
 
 if __name__ == '__main__':
